88da852faacc8bebbb4b51fc22c40c26d7208973
lihao <lihao@neolix.cn>
2025-04-25 16:34:30 +0800

    v0

--- web/src/views/Home.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <el-container style="height: 100vh">
-    <el-main>Hello World</el-main>
-  </el-container>
-</template>
-
-<script setup lang="ts"></script>
-
--- web/src/views/login/login.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="common-layout">
-    <el-container>
-      <el-main>Main</el-main>
-    </el-container>
-  </div>
-</template>
